ce55b2108e59a9c150e9a8153e91b44a5f7ab1b8
Daylend <drkcelestial@gmail.com>
2020-01-30 17:52:16 -0700

    Added f7 features and sillyness

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,25 @@
 var app = new Framework7({
-    root: "#app"
+    root: "#app",
+    routes: [
+        {
+            path: "/",
+            url: "index.html"
+        },
+        {
+            path: "/page2/",
+            url: "pages/page2.html"
+        }
+    ]
 });
 
 var mainView = app.views.create(".view-main");
+setInterval(function() {
+    document.getElementsByClassName("navbar-bg")[0].style.backgroundColor =
+        "rgb(" +
+        Math.random() * 255 +
+        "," +
+        Math.random() * 255 +
+        "," +
+        Math.random() * 255 +
+        ")";
+}, 5);
